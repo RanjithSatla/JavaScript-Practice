f6c2d0225420dd96006fec57d29b1b6eb6e81503
RanjithSatla <ranjithsatla327@gmail.com>
2022-03-14 23:52:14 +0530

    Closure Definitions

---
+++ Closures/closures.js
@@ -0,0 +1,10 @@
+///// CLOSURES ////
+
+/* Closure : Closure is the closed-over variabale environment of the execution context, even after that execution context is gone.
+
+2. A closure gives a function access to all the varibles of its parent function, even after that parent function has returned.The function keeps a refernce to its outer scope, which preserves the scope chain throughtout the time.
+
+3. A closure makes sure that a function doesn't loose connection to variables existed at the functions birth place.
+
+4. A closure is like a backpack that a function carries around whereever it goes. This backpack has all the variables that were present in the environment where the function was created.
+*/
