d2f87e916c22ec90132b7d7ba5ba8f4f91df9f33
Ranjith Satla <ranjithsatla327@gmail.com>
2022-05-10 16:19:47 +0530

    Value & Variables

---
+++ Fundamentals/values&variables.js
@@ -0,0 +1,76 @@
+//////  VALUES /////
+
+/*Values : A value is a peice of data. So it is the most basic unit of information that we have in programming.
+*/
+
+
+//////  VARIABLES /////
+
+/*
+A variable is created and a value is attached to it. It is created in the memory of the system and holds that value.
+*/
+
+
+// Naming Conventions
+
+/*
+
+1.Camel Case : When multiple words are their - first letter = Small , Later all First letters = Block.
+Ex: firstName
+
+2.Underscore : When multiple words are their - use underscore(_) in between the words.
+Ex: first_name
+ */
+
+
+
+////// DON'Ts //////
+
+
+/*
+
+1. Starting with a num
+Ex: let 3years = 3;
+console.log(years); // Variable declaration not allowed in this location
+
+2. Cannot use Other than : Letters, Underscore, $sign
+
+3.Cannot use reserved words in JS 
+Ex : let new = 27; // Variable declaration not allowed in this location
+
+4. Should not start with Uppercase : Not illegal but a convention.
+
+
+5. All UPPERCASE : Reserved for constantst
+Ex: PI = 3.14;
+*/
+
+
+
+///// DO's ////
+
+/*
+1.Name such that it explains the value easily;
+Ex : let myFirstJob = "Programmer";
+    let myCurrentJob = "Teacher"
+
+Instead of : let job1 = "Programmer";
+             let job2  = "Teacher"
+    */
+
+
+
+
+///// ASSIGNMENT //////
+
+/*
+1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console
+*/
+
+let country = "India";
+let continent = "Asia";
+let population = 131000000;
+
+console.log(country,continent,population); // India Asia 131000000
