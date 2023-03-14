9eb2902601856c034e59b4917472c9d8bce5d91e
Ranjith Satla <ranjithsatla327@gmail.com>
2023-03-14 12:14:00 +0530

    Closure Corner cases

--- Closures/closures.js
+++ Closures/closures.js
@@ -25,3 +25,31 @@ console.log(z); //[Function: y]
 z(); //7
 
 /* Now the function x and y are done with execution and removed out of EC. But the function returned has lexical scope bundled together when it is returned which has access to the variable a. */
+
+function x() {
+  let a = 7;
+
+  function y() {
+    console.log(a);
+  }
+  a = 100;
+  return y;
+}
+
+let p = x();
+console.log(p); //[Function: y]
+
+p(); //100 ; Because function p has the fun y and its lexical scope attached in which var a is not a value but the reference to var a.
+
+///// Uses of Closures :
+
+/*
+1.Module design Pattern.
+2.Currying.
+3.Memoize.
+4.Functions like once.
+5.Maintaining state in async world.
+6.setTimeouts.
+7.Iterators.
+and many more...
+*/
