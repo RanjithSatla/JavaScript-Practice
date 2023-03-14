87e2f0a24fec089df1da2ff4a43adc85d3be4755
Ranjith Satla <ranjithsatla327@gmail.com>
2023-03-14 11:59:41 +0530

    Block Scope

--- Hoisting/blockScope.js
+++ Hoisting/blockScope.js
@@ -35,3 +35,22 @@ console.log(a); //10
 // console.log(c);
 
 // Bcoz a is in Global scope and let and const are hoisted inside block scope so once initialised the block scope is removed and cant be accessed inside global scope.
+
+//////    SHADOWING  ////////
+
+var a = 11;
+let b = 21; // In script scope.
+const c = 31; // In script scope.
+
+{
+  var a = 111; // In Global scope shadows when value is initialised.
+  let b = 211; // In block scope
+  const c = 311; // In block scope
+  console.log(a); //111
+  console.log(b); //211
+  console.log(c); // 311
+}
+
+console.log(a); //111: This var 'a' shadows the 'a'in the global scope.
+console.log(b); //21
+console.log(c); //31
