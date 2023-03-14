9e606bbda868360c10c354c5b33b2a03150072d5
Ranjith Satla <ranjithsatla327@gmail.com>
2023-03-14 11:59:05 +0530

    Closure

--- Closures/closures.js
+++ Closures/closures.js
@@ -7,4 +7,21 @@
 3. A closure makes sure that a function doesn't loose connection to variables existed at the functions birth place.
 
 4. A closure is like a backpack that a function carries around whereever it goes. This backpack has all the variables that were present in the environment where the function was created.
+
+5.A function along with its lexical scope bundled together forms closure.
 */
+
+function x() {
+  let a = 7;
+
+  return function y() {
+    console.log(a);
+  };
+}
+
+let z = x();
+console.log(z); //[Function: y]
+
+z(); //7
+
+/* Now the function x and y are done with execution and removed out of EC. But the function returned has lexical scope bundled together when it is returned which has access to the variable a. */
