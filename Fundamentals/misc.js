de26883d1fe5c9a3bec53d0eba10c245f407ee4f
RanjithSatla <ranjithsatla327@gmail.com>
2025-02-25 08:50:00 +0530

    Minor changes

--- Fundamentals/misc.js
+++ Fundamentals/misc.js
@@ -1,62 +1,65 @@
-// Type Conversion : Converting manually from one datatype to another
+/// Type Conversion : Converting manually from one datatype to another
 
 const n1 = "23";
 const n2 = 20;
-console.log(n1+n2);  //2320
+console.log(n1 + n2); //2320
 
-console.log(Number(n1)+n2); //43 ie.., Number fun converted string to num.
+console.log(Number(n1) + n2); //43 ie.., Number fun converted string to num.
 
-const s1 = 23; // @3 as a num
+const s1 = 23; // 23 as a num
 console.log(String(23)); // 23 which is converted to a String here.
 
 console.log(Number("asd")); // NaN
-console.log(typeof(NaN));  // Number
+console.log(typeof NaN); // Number
 
+/// Type Coercion : JS internally converts one datatype to another.
 
-
-//Type Coercion : JS internally converts one datatype to another.
- 
+/*
+==> String plus Number = String
+==> Number plus Number = Number
+==> String plus String = String
+*/
 console.log("Iam" + "working" + "in" + "FloorNo" + 3); //IamworkinginFloorNo3 , ie.., Here 3 is converted into string.
 
-const n3 = "5", n4 = 6;
-console.log(n3+n4); //56
-console.log(n3-n4);  //1, ie.., n3 is comverted to num.
+const n3 = "5",
+  n4 = 6;
+console.log(n3 + n4); //56
+console.log(n3 - n4); //-1, ie.., n3 is converted to num.
 
+/* Here, JS converts n3 to number and performs the operation.
+==> string minus number = number
+*/
 
-//Falsy Values : 5 - 0 , Null, " ", Undefined, false.
+//Falsy Values : FIVE => 0 , Null, " ", Undefined, false.
 
 let height = 0;
 
-if(height) {
-    console.log("Height Provided by the user"); 
-}
-else {
-    console.log("Height not provided by the user");
+if (height) {
+  console.log("Height Provided by the user");
+} else {
+  console.log("Height not provided by the user");
 }
 
-//OP : Height not provided by the user ; Weird case which caused  due to type coerciosn as O is a falsy value.
-
+//OP : Height not provided by the user ; Weird case which caused  due to type coercion as O is a falsy value.
 
 /*
 /// Continue and Break  ////
 
-Continue : Continueb keyword in looping stops the current iteration and continue with the next iteration.
-Break : Break Keyword in looping terminates the looping completely.
+Continue : Continue keyword in looping stops the current iteration and continue with the next iteration.
+Break    : Break Keyword in looping terminates the looping completely.
 
 */
 
+const person = ["John", "Ivanka", 33, "USA"];
 
+//Log only strings :
 
-const person = ["John","Ivanka",33,"USA" ];
-
-//Log only strings : 
-
-for (let i=0;i<person.length;i++)  {
-    if(typeof person[i] != "string") continue;
-    console.log(person[i]);  // John  Ivanka  USA
+for (let i = 0; i < person.length; i++) {
+  if (typeof person[i] != "string") continue;
+  console.log(person[i]); // John  Ivanka  USA
 }
 
-for (let i=0;i<person.length;i++)  {
-    if(typeof person[i] == "number") break;
-    console.log(person[i]);   // John Ivanka
+for (let i = 0; i < person.length; i++) {
+  if (typeof person[i] == "number") break;
+  console.log(person[i]); // John Ivanka
 }
